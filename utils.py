5ddd2b6678492d9f8160be08a1883cb5cb9b0ec2
darina smirnova <darismi01@yandex.ru>
2024-04-15 12:39:29 +0300

    добавлена функция сортировки успешных операций улучшена функция открытия и загрузки данных из файла

--- utils.py
+++ utils.py
@@ -1,9 +1,26 @@
 import json
 
+
 # Функция открывает файл и загружает данные из него
 def load_file(filename):
-    file = open(filename, 'r')
-    json_loaded = json.load(file)
-    return json_loaded
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return data
+        # print(data)
+
+
+loaded_file = load_file('operations.json')
+
 
 # Фильтрация УСПЕШНЫХ операций
+def sort_executed(filename):
+    executed = []
+    for item in filename:
+        if item['state'] == "EXECUTED":
+            executed_oper = executed.append(item)
+
+    #return executed_oper
+    print(executed_oper)
+
+
+sort_executed(loaded_file)
